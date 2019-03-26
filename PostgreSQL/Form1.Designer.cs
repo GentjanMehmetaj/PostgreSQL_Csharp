c4f888b73eb6c08d3c99d611d5a6a3e50ddfb26f
Gentjan Mehmetaj <gent.mehmetaj@gmail.com>
2019-03-26 18:07:59 +0100

    add grid view and 2 button. Export from grid view to excel file.

--- PostgreSQL/Form1.Designer.cs
+++ PostgreSQL/Form1.Designer.cs
@@ -42,14 +42,18 @@
             this.meadlename_txt = new System.Windows.Forms.TextBox();
             this.studying_txt = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(21, 188);
+            this.button1.Location = new System.Drawing.Point(21, 220);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(268, 59);
+            this.button1.Size = new System.Drawing.Size(203, 59);
             this.button1.TabIndex = 0;
             this.button1.Text = "Get data  from table student";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,19 +62,19 @@
             // tbDataItems
             // 
             this.tbDataItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbDataItems.Location = new System.Drawing.Point(21, 269);
+            this.tbDataItems.Location = new System.Drawing.Point(21, 301);
             this.tbDataItems.Multiline = true;
             this.tbDataItems.Name = "tbDataItems";
             this.tbDataItems.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbDataItems.Size = new System.Drawing.Size(665, 205);
+            this.tbDataItems.Size = new System.Drawing.Size(491, 205);
             this.tbDataItems.TabIndex = 1;
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(370, 188);
+            this.button2.Location = new System.Drawing.Point(295, 220);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(301, 59);
+            this.button2.Size = new System.Drawing.Size(217, 59);
             this.button2.TabIndex = 2;
             this.button2.Text = "Get filtered data from table student";
             this.button2.UseVisualStyleBackColor = true;
@@ -164,7 +168,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(450, 6);
+            this.button3.Location = new System.Drawing.Point(291, 6);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(221, 64);
             this.button3.TabIndex = 10;
@@ -172,12 +176,43 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(745, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 44);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Export to fileExcel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(518, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(362, 313);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(518, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 44);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Export from DB to Grid View";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(698, 432);
+            this.ClientSize = new System.Drawing.Size(882, 508);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.studying_txt);
             this.Controls.Add(this.meadlename_txt);
@@ -195,6 +230,7 @@
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -216,6 +252,9 @@
         private System.Windows.Forms.TextBox meadlename_txt;
         private System.Windows.Forms.TextBox studying_txt;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- PostgreSQL/Form1.cs
+++ PostgreSQL/Form1.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 
+using _excel = Microsoft.Office.Interop.Excel;
 using Npgsql;
-
+using System.Data;
 
 namespace PgSql
 {
@@ -69,6 +70,68 @@ namespace PgSql
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {    // creating Excel Application  
+            _excel._Application app = new _excel.Application();
+            // creating new WorkBook within Excel application  
+            _excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            // creating new Excelsheet in workbook  
+            _excel._Worksheet worksheet = null;
+            // see the excel sheet behind the program  
+            app.Visible = true;
+            // get the reference of first sheet. By default its name is Sheet1.  
+            // store its reference to worksheet  
+            worksheet = workbook.Sheets["Sheet1"];
+            worksheet = workbook.ActiveSheet;
+            // changing the name of active sheet  
+            worksheet.Name = "Exported from gridview";
+            // storing header part in Excel  
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            // storing Each row and column value to excel sheet  
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            // save the application  
+            workbook.SaveAs(@"C:\Users\albana\Desktop\output.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, _excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            // Exit from the application  
+            app.Quit();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string connstring = "Server=127.0.0.1; Port=5432; User Id=postgres; " +
+                   "Password=b2b4cc1b2; Database=DataStudent;";
+            NpgsqlConnection connection = new NpgsqlConnection(connstring);
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM public.student", connection);
+
+            try
+            {
+
+                NpgsqlDataAdapter NpgsqlDA = new NpgsqlDataAdapter();
+
+                NpgsqlDA.SelectCommand = command;
+                DataTable dbdataset = new DataTable();
+                NpgsqlDA.Fill(dbdataset);
+                BindingSource bsource = new BindingSource();
+                bsource.DataSource = dbdataset;
+                dataGridView1.DataSource = bsource;
+                NpgsqlDA.Update(dbdataset);
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.Message);
+
             }
         }
     }
